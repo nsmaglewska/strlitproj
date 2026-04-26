0dcee4d7764b6157793aa07c70e2a475be3d7de8
nsmaglewska <95300180+nsmaglewska@users.noreply.github.com>
2026-04-26 14:11:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,21 +10,6 @@ st.title("Gemini chatbot app")
 api_key, base_url = st.secrets["API_KEY"], st.secrets["BASE_URL"]
 selected_model = "gemini-2.5-flash"
 
-def load_pdf(file_path):
-    doc = fitz.open(file_path)
-    text = ""
-    for page in doc:
-        text += page.get_text()
-    doc.close()
-    return text
-def load_documents_from_folder(folder_path):
-    documents = []
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".pdf"):
-            text = load_pdf(os.path.join(folder_path, filename))
-            documents.append({"filename:" filename, "text:" text})
-    return documents
-
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?."}]
 
