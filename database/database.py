5e63ab50410218de1154f2a690f6072da97ea79c
nsmaglewska <95300180+nsmaglewska@users.noreply.github.com>
2026-07-09 13:29:18 +0200

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,7 +1,8 @@
 import sqlite3
 import os
 DATABASE = "database/foods.db"
-def search_food(food_name):
+def search_food(name):
+    name = name.lower().strip()
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
@@ -9,13 +10,13 @@ def search_food(food_name):
         """
         SELECT *
         FROM foods
-        WHERE LOWER(name) LIKE LOWER(?)
+        WHERE LOWER(name) LIKE ?
         LIMIT 1
         """,
-        (f"%{food_name}%",)
+        (
+            f"%{name}%",
+        )
     )
     row = cursor.fetchone()
     conn.close()
-    if row:
-        return dict(row)
-    return None
+    return dict(row) if row else None
