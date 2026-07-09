6cb0aa35571915aaa57688b0fe05ae1665602249
nsmaglewska <95300180+nsmaglewska@users.noreply.github.com>
2026-07-09 12:58:30 +0200

    Create nutrition_analyzer.py

---
+++ nutrition_analyzer.py
@@ -0,0 +1,28 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+template = """
+Jesteś dietetykiem.
+
+Przeanalizuj poniższy produkt spożywczy.
+
+{context}
+
+Odpowiedz na pytania:
+
+1. Czy produkt jest zdrowy?
+2. Jakie są jego zalety?
+3. Jakie są jego wady?
+4. Dla kogo jest polecany?
+5. Czy nadaje się na redukcję masy ciała?
+6. Oceń produkt w skali 1-10 i uzasadnij ocenę.
+
+Odpowiedź:
+"""
+
+def analyze_nutrition(context, model):
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | model
+    response = chain.invoke({
+        "context": context
+    })
+    return response
