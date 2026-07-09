9472be16ffd0bae056edacf0a99bd1da3456cebf
nsmaglewska <95300180+nsmaglewska@users.noreply.github.com>
2026-07-09 13:24:14 +0200

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,23 +1,17 @@
 import sqlite3
 
-
 DATABASE = "database/foods.db"
-
-
 
 def search_food(name):
 
-    conn = sqlite3.connect(
-        DATABASE
-    )
+    conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     cursor.execute(
         """
         SELECT *
         FROM foods
-        WHERE LOWER(name)
-        LIKE LOWER(?)
+        WHERE LOWER(name) LIKE LOWER(?)
         LIMIT 1
         """,
         (
@@ -29,20 +23,3 @@ def search_food(name):
     if row:
         return dict(row)
     return None
-
-def check_database():
-
-    conn = sqlite3.connect(DATABASE)
-
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "SELECT name FROM sqlite_master WHERE type='table';"
-    )
-
-    print(cursor.fetchall())
-
-    conn.close()
-
-
-check_database()
