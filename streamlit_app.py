f9ee020b98d07e2610cc1f23a558fa6ff7d7e2ce
nsmaglewska <95300180+nsmaglewska@users.noreply.github.com>
2026-07-09 12:32:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,15 @@ from food_detector import detect_food
 
 # a
 
-st.set_page_config(layout="wide", page_title="Gemini chatbot app")
-st.title("Gemini chatbot app")
+st.set_page_config(layout="wide", page_title="Food nutrition app")
+st.title("Food nutrition app")
 
 # api_key, base_url = os.environ["API_KEY"], os.environ["BASE_URL"]
 api_key, base_url = st.secrets["API_KEY"], st.secrets["BASE_URL"]
 selected_model = "gemini-2.5-flash"
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?."}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "W czym mogę pomóc?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
