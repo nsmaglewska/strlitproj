9e3932f7d36c25e344cb1fbab49c2bd722961a29
nsmaglewska <95300180+nsmaglewska@users.noreply.github.com>
2026-07-09 13:12:00 +0200

    Update import_foods.py

--- database/import_foods.py
+++ database/import_foods.py
@@ -1,23 +1,39 @@
 import requests
 
 def get_food_data(product_name):
-    url = "https://world.openfoodfacts.org/cgi/search.pl"
+
+    url = "https://api.nal.usda.gov/fdc/v1/foods/search"
 
     params = {
-        "search_terms": product_name,
-        "search_simple": 1,
-        "action": "process",
-        "json": 1
+        "api_key": API_KEY,
+        "query": product_name,
+        "pageSize": 1
     }
 
-    response = requests.get(url, params=params)
+    response = requests.get(
+        url,
+        params=params
+    )
 
     if response.status_code != 200:
         return None
 
     data = response.json()
 
-    if len(data["products"]) == 0:
+    if not data["foods"]:
         return None
 
-    return data["products"][0]
+    food = data["foods"][0]
+
+    nutrients = {}
+
+    for nutrient in food["foodNutrients"]:
+        name = nutrient.get("nutrientName")
+        value = nutrient.get("value")
+
+        nutrients[name] = value
+
+    return {
+        "name": food["description"],
+        "nutrients": nutrients
+    }
