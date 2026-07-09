4d406980a066161663d75fd3682e8c417eb8c68a
nsmaglewska <95300180+nsmaglewska@users.noreply.github.com>
2026-07-09 13:35:07 +0200

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,66 +1,22 @@
-#import sqlite3
-#import os
-#DATABASE = "database/foods.db"
-#def search_food(name):
-#    name = name.lower().strip()
-#    conn = sqlite3.connect(DATABASE)
-#    conn.row_factory = sqlite3.Row
-#    cursor = conn.cursor()
-#    cursor.execute(
-#        """
-#        SELECT *
-#        FROM foods
-#        WHERE LOWER(name) LIKE ?
-#        LIMIT 1
-#        """,
-#        (
-#            f"%{name}%",
-#        )
-#    )
-#    row = cursor.fetchone()
-#    conn.close()
-#    return dict(row) if row else None
 import sqlite3
 import os
-
 DATABASE = "database/foods.db"
-
-
 def search_food(name):
-
-    print("DATABASE:", os.path.abspath(DATABASE))
-
+    name = name.lower().strip()
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
-
     cursor = conn.cursor()
-
-    # sprawdzenie tabel
     cursor.execute(
-        "SELECT name FROM sqlite_master WHERE type='table';"
+        """
+        SELECT *
+        FROM foods
+        WHERE LOWER(name) LIKE ?
+        LIMIT 1
+        """,
+        (
+            f"%{name}%",
+        )
     )
-
-    print("TABLES:", cursor.fetchall())
-
-
-    # sprawdzenie struktury foods
-    cursor.execute(
-        "PRAGMA table_info(foods);"
-    )
-
-    print("COLUMNS:", cursor.fetchall())
-
-
-    # test pobrania danych
-    cursor.execute(
-        "SELECT * FROM foods LIMIT 1"
-    )
-
-    test = cursor.fetchone()
-
-    print("FIRST ROW:", test)
-
-
+    row = cursor.fetchone()
     conn.close()
-
-    return dict(test) if test else None
+    return dict(row) if row else None
